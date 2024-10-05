b5c9c2d355ddf693aeaae4852d91e4b660ee92b0
Matthew-Hill2000 <124274792+Matthew-Hill2000@users.noreply.github.com>
2024-10-05 13:00:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="language_model",
+    name="rnn_text_generator",
     version="1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
-        "torch",
-        "numpy"
-        "tensorboard"
+        "torch>=1.9.0",
+        "numpy>=1.19.5",
+        "tensorboard>=2.6.0",
     ],
     entry_points={
         'console_scripts': [
-            'rnn_text_generator = scripts/run:run',
+            'run_rnn_generator=scripts.run:run',
         ],
     },
-    author='Matthew Hill',
-    description='RNN Text Generator for sequence prediction tasks',
+    python_requires=">=3.7",
+    author="Matthew Hill",
+    author_email="mattiejhill@gmail.com@example.com",
+    description="A RNN-based text generator",
     long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/Matthew-Hill2000/rnn_text_generator',  # GitHub URL
+    long_description_content_type="text/markdown",
+    url="https://github.com/Matthew-Hill2000/rnn_text_generator",
 )
